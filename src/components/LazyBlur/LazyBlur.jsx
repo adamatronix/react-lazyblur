a9c3e83a4c80b9df516d1f253c125281fdba2cb9
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-04 11:28:34 +0800

    setup Copy over lazy blur component from Post

---
+++ src/components/LazyBlur/LazyBlur.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import LazyLoad from 'react-lazyload';
+import { Transition } from 'react-transition-group';
+import classNames from 'classnames/bind';
+import "./styles/lazy-blur.scss";
+
+const LazyBlur = props => {
+    const [Loaded, setLoaded] = useState(false);
+    const [childrenEl, setChildren] = useState(false);
+    const { placeholder, placeholderWidth, placeholderHeight, offset, children} = props;
+    const duration = 200;
+
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease`,
+        opacity: 0,
+      }
+      
+    const transitionStyles = {
+    entering: { opacity: 1 },
+    entered:  { opacity: 1 },
+    exiting:  { opacity: 0 },
+    exited:  { opacity: 0 },
+    };
+    
+    let ratio = (placeholderHeight / placeholderWidth) * 100;
+
+    let wrapperStyles = {
+        position: "relative",
+        width: "100%",
+        paddingBottom: ratio + "%",
+        overflow: "hidden"
+    };
+
+    let placeholderStyles = {
+        opacity: 1,
+    };
+
+    const placeholderTransitionStyles = {
+        entering: { opacity: 1 },
+        entered:  { opacity: 0 },
+        exiting:  { opacity: 1 },
+        exited:  { opacity: 1 },
+        };
+
+    useEffect(() => {
+        let childrenCopies = React.Children.map(children, item => {
+
+            let compName = item.type.name;
+            let classes, props, imageprops;
+            
+            if(item.props.zoomImage) {
+                classes = classNames(item.props.image.className, {
+                    "lazy-blur": true
+                });    
+                imageprops = { ...item.props.image, className: classes, onLoad: onLoad };
+                props = { ...item.props, image: imageprops };
+            } else {
+                classes = classNames(item.props.className, {
+                    "lazy-blur": true
+                });
+                props = { ...item.props, className: classes, onLoad: onLoad };
+            }
+            return React.cloneElement(item, props);
+        });
+        setChildren(childrenCopies);
+
+    },[]);
+
+    
+
+    function onLoad() {
+        setLoaded(true);
+    }
+
+    return (
+        <Transition in={Loaded} timeout={duration}>
+        {state => ( <div style={wrapperStyles}>
+            <ImagePlaceholder style={{
+                    ...placeholderStyles,
+                    ...placeholderTransitionStyles[state]
+                }} src={placeholder} />
+            <LazyLoad offset={ (offset) ? offset : -250 } placeholder={<ImagePlaceholder src={placeholder}/>} once>
+                <div style={{
+                    ...defaultStyle,
+                    ...transitionStyles[state]
+                }}>
+                    { childrenEl ? childrenEl : ""}
+                </div>
+                  
+            </LazyLoad>
+        </div>)}  
+        </Transition>
+    )
+
+}
+
+const ImagePlaceholder = props => {
+    const { src, style } = props;
+
+    let styles = {
+        left: "0",
+        top: "0",
+        position: "absolute",
+        width: "100%",
+        height: "100%"
+    };
+
+    return (
+        <React.Fragment>
+            <img style={{...styles, ...style}} src={src} />
+        </React.Fragment>
+    )
+
+}
+export default LazyBlur;
