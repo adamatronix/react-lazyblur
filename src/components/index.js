afcce04e37acc4fc55a1ad8a7da60bbc3ad4205a
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-05 11:40:01 +0800

    export-bg Export the background component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,12 @@
 // components/index.js
 
 import LazyBlur from './LazyBlur/LazyBlur';
+import LazyBackgroundBlur from './LazyBlur/LazyBackgroundBlur';
 import LazyBlurImage from './LazyBlur/LazyBlurImage';
 
 export { 
     LazyBlur,
+    LazyBackgroundBlur,
     LazyBlurImage
 };
+å
