94adb0221d98f54b235cacde6026b0e5dbbea549
Your Name <you@example.com>
2021-06-18 15:03:21 +0800

    blurhash Include blurhash dependency and create blurhash placeholder which can either accept a hash or fetch a hash from a url

--- src/components/LazyBlur/LazyBlur.stories.js
+++ src/components/LazyBlur/LazyBlur.stories.js
@@ -94,3 +94,57 @@ export const Default = () => {
     </>
   );
 }
+
+export const BlurHash = () => {
+  return (
+    <>
+    <div style={{position: 'relative', display: 'grid', gridColumnGap: '30px', gridTemplateColumns: 'repeat(12,1fr)'}}>
+      <div style={{gridColumn: '6 / span 6'}}>
+        <LazyBlur 
+          blurhash={{
+            src: "https://images.prismic.io/adamatronix/268a9aba-ce78-488e-aeeb-622fc8e587cf_20201219_DevHynes_00203.jpeg?fm=blurhash&w=50"
+          }}
+          placeholderWidth={ImageData.image.sizes['medium_large-width']} 
+          placeholderHeight={ImageData.image.sizes['medium_large-height']}>
+            <img style={{width: "100%"}} src={ImageData.image.sizes['medium_large']} />
+        </LazyBlur>
+      </div>
+    </div>
+    <div style={{position: 'relative', margin: '800px 0', display: 'grid', gridColumnGap: '30px', gridTemplateColumns: 'repeat(12,1fr)'}}>
+      <div style={{gridColumn: '1 / span 6'}}>
+        <LazyBlur 
+          blurhash={{
+            src: "https://images.prismic.io/adamatronix/c1e1365f-6e9c-4f2f-aecf-0b19110b97af_20201219_DevHynes_00215.jpeg?fm=blurhash&w=50"
+          }}
+          placeholderWidth={ImageDataTwo.image.sizes['preload-width']} 
+          placeholderHeight={ImageDataTwo.image.sizes['preload-height']}>
+            <img style={{width: "100%"}} src={ImageDataTwo.image.sizes['medium_large']} />
+        </LazyBlur>
+      </div>
+    </div>
+    <div style={{position: 'relative', margin: '800px 0', display: 'grid', gridColumnGap: '30px', gridTemplateColumns: 'repeat(12,1fr)'}}>
+      <div style={{gridColumn: '1 / span 12'}}>
+        <LazyBlur 
+          blurhash={{
+            src: "https://images.prismic.io/adamatronix/f5031b85-4731-4078-a192-5d319ba5603d_Blood-Orange-billboard.jpeg?fm=blurhash&w=50"
+          }}
+          placeholder={ImageDataThree.image.sizes['preload']} 
+          placeholderWidth={ImageDataThree.image.sizes['medium_large-width']} 
+          placeholderHeight={ImageDataThree.image.sizes['medium_large-height']}>
+            <img style={{width: "100%"}} src={ImageDataThree.image.sizes['medium_large']} />
+        </LazyBlur>
+      </div>
+    </div>
+    <div style={{position: 'relative', margin: '800px 0', display: 'grid', gridColumnGap: '30px', gridTemplateColumns: 'repeat(12,1fr)'}}>
+      <div style={{gridColumn: '4 / span 6'}}>
+        <LazyBlur 
+          placeholder={ImageData.image.sizes['preload']} 
+          placeholderWidth={ImageData.image.sizes['medium_large-width']} 
+          placeholderHeight={ImageData.image.sizes['medium_large-height']}>
+            <img style={{width: "100%"}} src={ImageData.image.sizes['medium_large']} />
+        </LazyBlur>
+      </div>
+    </div>
+    </>
+  );
+}
