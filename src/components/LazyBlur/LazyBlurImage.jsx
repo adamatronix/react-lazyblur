c4d790bf42d957b7acd754b807db851254f9959e
adamatronix <you@example.com>
2022-05-17 20:33:49 +0800

    object-fit-image Add object fit prop to lazy blur image component

--- src/components/LazyBlur/LazyBlurImage.jsx
+++ src/components/LazyBlur/LazyBlurImage.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import LazyBlur from './LazyBlur';
 
 const LazyBlurImage = (props) => {
-  const { placeholder, offset, className, src, alt} = props;
+  const { placeholder, offset, className, src, alt, ...rest} = props;
 
   return (
     <LazyBlur
       offset={offset}
       placeholder={placeholder.src} 
       placeholderWidth={placeholder.width} 
-      placeholderHeight={placeholder.height}>
+      placeholderHeight={placeholder.height}
+      {...rest}>
         <img style={{width: "100%"}} className={className} src={src} alt={alt}/>
     </LazyBlur>
   )
